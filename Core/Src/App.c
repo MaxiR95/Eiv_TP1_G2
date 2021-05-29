a5e0b9aa3b9875e20e1559f400e633ff11984ea9
Nicolas Miguel Maximiliano Reyes <maximiliano.reyes17@gmail.com>
2021-05-29 19:56:22 -0300

    Correccion nombres de los pines de App

--- Core/Src/App.c
+++ Core/Src/App.c
@@ -17,11 +17,11 @@ void loop(void) {
 	//HAL_Delay(500);
 
 	//Lee boton en A1
-	if (HAL_GPIO_ReadPin(Bot_A1_GPIO_Port, Bot_A1_Pin) == GPIO_PIN_RESET) {
+	if (HAL_GPIO_ReadPin(pulsador_GPIO_Port, pulsador_Pin) == GPIO_PIN_RESET) {
 		//si boton presionado, enciende led D1
-		HAL_GPIO_WritePin(Led_D1_GPIO_Port, Led_D1_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_RESET);
 	} else {
 		//sino, apaga led D1
-		HAL_GPIO_WritePin(Led_D1_GPIO_Port, Led_D1_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_SET);
 	}
 }
