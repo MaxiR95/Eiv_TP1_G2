32c906ccda0fdb4bc59422e251cb581b95754b29
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2021-05-29 21:52:11 -0300

    Implementación básica de objeto Pin

--- Core/Src/App.c
+++ Core/Src/App.c
@@ -7,21 +7,49 @@
 
 #include <main.h>
 
-void setup(void) {
 
+
+/* Pin */
+/*-----struct-----*/
+typedef struct Pin{
+	GPIO_TypeDef *gpio;
+	uint16_t pin;
+}Pin;
+/*-----funciones-----*/
+void Pin_init(Pin *self, GPIO_TypeDef *gpio, uint16_t pin)
+{
+	self->gpio=gpio;
+	self->pin=pin;
 }
+int Pin_leer(Pin *self)
+{
+	return HAL_GPIO_ReadPin(self->gpio, self->pin);
+}
+void Pin_escribir(Pin *self,int valor)
+{
+	HAL_GPIO_WritePin(self->gpio, self->pin, valor);
+}
+/*-----fin----------*/
 
+
+
+
+
+static Pin pulsador;
+static Pin led;
+
+void setup(void) {
+	Pin_init(&pulsador,pulsador_GPIO_Port,pulsador_Pin);
+	Pin_init(&led,Led_GPIO_Port,Led_Pin);
+}
 void loop(void) {
-	//enciende/apaga led D1, cada 500ms
-	//HAL_GPIO_TogglePin(Led_D1_GPIO_Port, Led_D1_Pin);
-	//HAL_Delay(500);
-
 	//Lee boton en A1
-	if (HAL_GPIO_ReadPin(pulsador_GPIO_Port, pulsador_Pin) == GPIO_PIN_RESET) {
+	if (Pin_leer(&pulsador) == 0) {
 		//si boton presionado, enciende led D1
-		HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_RESET);
+		Pin_escribir(&led, 0);
 	} else {
 		//sino, apaga led D1
-		HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_SET);
+		Pin_escribir(&led, 1);
 	}
+
 }
