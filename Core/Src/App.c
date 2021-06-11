73229155d6e8d9711a2cd5bfdf6c140b6525653c
Juan Raul Viveros Coria <juan@exo_6745>
2021-06-11 10:13:07 -0300

    CHecho-inicialización de pines , init de R595

--- Core/Src/App.c
+++ Core/Src/App.c
@@ -75,11 +75,9 @@ void R595_actualizaSalidas(R595 *self);
 void R595_init(R595 *self,Pin *dato, Pin *reloj, Pin *cerrojo)
 {
 	/* REEMPLAZAR POR UNA IMPLEMENTACIÓN QUE FUNCIONE */
-	/*stub - implementación vacía, reemplazar con la verdadera implementación*/
-	(void)self;
-	(void)dato;
-	(void)reloj;
-	(void)cerrojo;
+	self->dato=dato;
+	self->reloj=reloj;
+	self->cerrojo=cerrojo;
 }
 void R595_ingresaByte(R595 *self,uint8_t byte)
 {
@@ -111,10 +109,15 @@ void setup(void) {
 	Pin_init(&pulsador,pulsador_GPIO_Port,pulsador_Pin);
 	Pin_init(&led,Led_GPIO_Port,Led_Pin);
 	/* HACER: Terminar inicialización pines*/
+	Pin_init(&data,data_GPIO_Port,data_Pin);
+	Pin_init(&clock,clock_GPIO_Port,clock_Pin);
+	Pin_init(&latch,latch_GPIO_Port,latch_Pin);
 
 	/*Inicializar driver registro de desplazamiento*/
 	R595_init(&registroDisplay, &data, &clock, &latch);
 }
+
+
 void loop(void) {
 	uint8_t config_catodos = 0xFF; // por defecto - todo apagado
 	uint8_t config_anodos = 0; // por defecto - todo apagado
